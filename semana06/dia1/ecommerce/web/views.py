10a75501efea5ac9e8bdf587faceb0910ee50192
JesusChiro <jesus_chiroque@hotmail.com>
2023-06-22 13:07:29 -0500

    dia2 categorias

--- semana06/dia1/ecommerce/web/views.py
+++ semana06/dia1/ecommerce/web/views.py
@@ -5,7 +5,25 @@ from .models import Categoria, Marca, Producto
 # Create your views here.
 def index(request):
     lista_productos = Producto.objects.all()
+    lista_categorias = Categoria.objects.all()
+    lista_marcas = Marca.objects.all()
     context = {
         "productos": lista_productos,
+        "categorias": lista_categorias,
+        "marcas": lista_marcas,
+    }
+    return render(request, "index.html", context)
+
+
+def productos_por_categoria(request, categoria_id):
+    obj_categoria = Categoria.objects.get(pk=categoria_id)
+    # lista_productos = Producto.objects.filter(categoria_id=obj_categoria)
+    lista_productos = obj_categoria.producto_set.all()
+    lista_categorias = Categoria.objects.all()
+    lista_marcas = Marca.objects.all()
+    context = {
+        "productos": lista_productos,
+        "categorias": lista_categorias,
+        "marcas": lista_marcas,
     }
     return render(request, "index.html", context)

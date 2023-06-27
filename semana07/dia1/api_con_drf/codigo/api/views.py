bed4cc02f61a3f188f8577b8047d4cb429358b12
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-06-26 22:34:56 -0500

    drf get y post con drf

--- semana07/dia1/api_con_drf/codigo/api/views.py
+++ semana07/dia1/api_con_drf/codigo/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Alumno
+from .serializer import AlumnoSerializer
 
 @api_view(['GET'])
 def index(request):
@@ -9,3 +11,23 @@ def index(request):
     }
     
     return Response(context)
+
+@api_view(['GET', 'POST'])
+def alumnon(request):
+    if request.method == 'GET':
+        lista_alumnos=Alumno.objects.all()
+        ser_alumnos=AlumnoSerializer(lista_alumnos,many=True)
+        context={
+            'status': True,
+            'content': ser_alumnos.data
+        }
+    elif request.method == 'POST':
+        ser_alumno=AlumnoSerializer(data=request.data)
+        ser_alumno.is_valid(raise_exception=True)
+        obj_nuevo_alumno=ser_alumno.objects.all()
+        context={
+            'status': True,
+            'content': AlumnoSerializer(obj_nuevo_alumno).data
+        }
+        
+        return Response(context)

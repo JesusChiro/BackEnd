6ba92863db76bc1daba5f7c2dd41f5fc5b2e2013
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-06-26 22:12:24 -0500

    drf basico

--- semana07/dia1/api_con_drf/codigo/codigo/urls.py
+++ semana07/dia1/api_con_drf/codigo/codigo/urls.py
@@ -1,4 +1,4 @@
-"""cursos URL Configuration
+"""codigo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -18,6 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',include('api.urls')),
-    path('peliculas/',include('peliculas.urls')),
     path('admin/', admin.site.urls),
 ]

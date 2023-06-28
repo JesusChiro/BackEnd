439d5bb6cd06197089b2ebfbf2f9ec91a893ff1f
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-06-27 19:43:40 -0500

    dia2

---
+++ semana07/dia2/todolist_api/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class IndexView(APIView):
+    def get(self, request):
+        context={
+            'status': True,
+            'content':'servidor activo'
+        }
+        return Response(context)

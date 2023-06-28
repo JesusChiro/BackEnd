5edf67631953f885d6ec0e1d84bebbd5a9883ff5
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-06-27 21:33:31 -0500

    dia2 get con pk

--- semana07/dia2/todolist_api/api/models.py
+++ semana07/dia2/todolist_api/api/models.py
@@ -5,12 +5,12 @@ class Tarea(models.Model):
     
     ESTADO_CHOICES = (
         ('pendiente','PENDIENTE'),
-        ('terminado','TERMINADO'),
+        ('terminado','TERMINADO')
     )
     
     descripcion = models.CharField(max_length=200)
-    fecha_registro=models.DateField(auto_now_add=True)
-    estado=models.CharField(max_length=20,choices=ESTADO_CHOICES)
+    fecha_registro = models.DateField(auto_now_add=True)
+    estado = models.CharField(max_length=20,choices=ESTADO_CHOICES,default='pendiente')
     
     def __str__(self):
         return self.descripcion
--- semana07/dia2/todolist_api/todolist_api/settings.py
+++ semana07/dia2/todolist_api/todolist_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-euv+k+-n^*8$y8@fvb%*5$^9gpzcww9ka@nqnt9=rujr^&(#@y'
+SECRET_KEY = 'django-insecure-vj=g#fa5z)r^ws@rx^ddx@=46%2_8710i$giajk#i#g*a_ixpl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

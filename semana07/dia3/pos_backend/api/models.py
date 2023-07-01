a3f1116cf5cd40ab79c6eaed64c254f2cbccda7d
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-06-30 20:02:07 -0500

    dia4 pedidos modelos

--- semana07/dia3/pos_backend/api/models.py
+++ semana07/dia3/pos_backend/api/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 from cloudinary.models import CloudinaryField
@@ -37,13 +38,58 @@ class Plato(models.Model):
                                     default=0,
                                     verbose_name='Precio')
     categoria_id = models.ForeignKey(Categoria,
-                                    related_name='Platos',
-                                    db_column='categoria_id',
-                                    on_delete=models.RESTRICT,
-                                    verbose_name='Categoria')
+                                     related_name='Platos',
+                                     db_column='categoria_id',
+                                     on_delete=models.RESTRICT,
+                                     verbose_name='Categoria')
 
     class Meta:
         db_table = 'tbl_plato'
 
     def __str__(self):
         return self.plato_nom
+
+
+class Pedido(models.Model):
+    ESTADO_CHOICES = (
+        ('solicitado', 'SOLICITADO'),
+        ('entregado', 'ENTREGADO')
+    )
+    pedido_id = models.AutoField(primary_key=True)
+    pedido_fech = models.DateTimeField(null=True)
+    pedido_nro = models.CharField(max_length=100)
+    pedido_est = models.CharField(max_length=100,
+                                  default='solicitado',
+                                  choices=ESTADO_CHOICES)
+    mesa_id = models.ForeignKey(Mesa,
+                                to_field='mesa_id',
+                                on_delete=models.RESTRICT,
+                                db_column='mesa_id')
+    usu_id = models.ForeignKey(User,
+                               to_field='id',
+                               related_name='Pedidos',
+                               on_delete=models.RESTRICT,
+                               db_column='usu_id')
+
+    class Meta:
+        db_table = 'tbl_pedido'
+
+    def __str__(self):
+        return self.pedido_nro
+
+
+class PedidoPlato(models.Model):
+    pedidoplato_id = models.AutoField(primary_key=True)
+    pedidoplato_cant = models.IntegerField(default=1)
+    plato_id = models.ForeignKey(Plato, related_name='pedidoplatos',
+                                 to_field='plato_id', db_column='plato_id',
+                                 on_delete=models.RESTRICT)
+    pedido_id = models.ForeignKey(Pedido, related_name='pedidoplatos',
+                                  to_field='pedido_id', db_column='pedido_id',
+                                  on_delete=models.RESTRICT)
+
+    class Meta:
+        db_table = 'tbl_pedido_plato'
+
+    def __str__(self):
+        return (self.plato_id)

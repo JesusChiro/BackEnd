aab5e55eba0ad6c9d1d0b37324effb0f919b7eeb
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-07-04 22:12:13 -0500

    tareas get

---
+++ semana08/dia2/proyecto-tareas/database.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql')
+
+const mysqlConnection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'db_tareas_g20',
+    port:'3306'
+})
+
+mysqlConnection.connect(function(err){
+    if(err){
+        console.error(err)
+        return
+    }else{
+        console.log('conectado a bd')
+    }
+})
+
+module.exports = mysqlConnection
---
+++ semana08/dia2/proyecto-tareas/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mysqlConnection = require('./database')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        'status': true,
+        'content': 'api rest activo'
+    })
+})
+
+app.get('/tarea', (req, res) => {
+    mysqlConnection.query("select * from tarea",
+        (err, rows, fields) => {
+            if (!err) {
+                context = {
+                    'status': true,
+                    'content': rows
+                }
+                res.json(context)
+            } else {
+                console.log(err)
+            }
+        })
+})
+
+app.listen(5000, () => console.log('http://localhost:5000'))

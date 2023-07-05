1122d42526589ee27d63db42a8a4a2c206055738
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-07-04 22:56:30 -0500

    dia 2 tareas delete

--- semana08/dia2/proyecto-tareas/index.js
+++ semana08/dia2/proyecto-tareas/index.js
@@ -3,6 +3,8 @@ const mysqlConnection = require('./database')
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.json({
         'status': true,
@@ -19,6 +21,72 @@ app.get('/tarea', (req, res) => {
                     'content': rows
                 }
                 res.json(context)
+            } else {
+                console.log(err)
+            }
+        })
+})
+
+app.post('/tarea', (req, res) => {
+    const { descripcion, estado } = req.body
+    const query = `insert into tarea(descripcion,estado) values('${descripcion}', '${estado}')`
+
+    mysqlConnection.query(query, (err, rows, fields) => {
+        if (!err) {
+            res.json({
+                'status': true,
+                'content': 'registro exitoso'
+            })
+        } else {
+            console.log(err)
+        }
+    })
+})
+
+app.get('/tarea/:id', (req, res) => {
+    const { id } = req.params
+    const query = `select * from tarea where id = ?`
+
+    mysqlConnection.query(query, [id], (err, rows, fields) => {
+        if (!err) {
+            res.json(rows)
+        } else {
+            console.log(err)
+        }
+    })
+})
+
+app.put('/tarea/:id', (req, res) => {
+    const { id } = req.params
+    const { descripcion, estado } = req.body
+    const query = `update tarea set 
+                   descripcion=?,estado=? 
+                   where id=?`
+
+    mysqlConnection.query(query, [descripcion, estado, id],
+        (err, rows, fields) => {
+            if (!err) {
+                res.json({
+                    'status': true,
+                    'content': 'registro actualizado con exito'
+                })
+            } else {
+                console.log(err)
+            }
+        })
+})
+
+app.delete('/tarea/:id', (req, res) => {
+    const { id } = req.params
+    const query = `delete from tarea where id =?`
+
+    mysqlConnection.query(query, [id],
+        (err, rows, fields) => {
+            if (!err) {
+                res.json({
+                    'status': true,
+                    'content': 'registro eliminado con exito'
+                })
             } else {
                 console.log(err)
             }

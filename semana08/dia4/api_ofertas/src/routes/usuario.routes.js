7d61b897c362e60f5ccd33ecbcffce5b773ba0f2
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-07-07 19:56:34 -0500

    dia4 auth middleware

--- semana08/dia4/api_ofertas/src/routes/usuario.routes.js
+++ semana08/dia4/api_ofertas/src/routes/usuario.routes.js
@@ -1,49 +1,49 @@
 const express = require('express')
 const UsuarioService = require('../services/usuario.service')
 const jwt = require('jsonwebtoken')
-const {config} = require('../config')
+const { config } = require('../config')
 
 const boom = require('@hapi/boom')
 
-function usuarioApi(app){
+function usuarioApi(app) {
     const router = express.Router()
-    app.use('/usuario',router)
+    app.use('/usuario', router)
 
     const objUsuario = new UsuarioService()
 
-    router.post('/',async function (req,res){
-        const {body: usuario} = req
-        try{
-            const data = await objUsuario.create({usuario})
+    router.post('/', async function (req, res) {
+        const { body: usuario } = req
+        try {
+            const data = await objUsuario.create({ usuario })
             res.status(201).json({
-                status:true,
-                content:data
+                status: true,
+                content: data
             })
-        }catch(err){
+        } catch (err) {
             res.status(500).json(boom.badData(err))
         }
     })
 
-    router.post('/login',async function (req,res){
-        const {body: usuario} = req
+    router.post('/login', async function (req, res) {
+        const { body: usuario } = req
 
-        const authUsuario = await objUsuario.authenticate({usuario})
-        if(authUsuario.id > 0){
+        const authUsuario = await objUsuario.authenticate({ usuario })
+        if (authUsuario.id > 0) {
             const token = jwt.sign(
                 authUsuario,
                 config.jwt_secret,
                 {
-                    expiresIn:'1h'
+                    expiresIn: '1m'
                 }
             )
             res.status(200).json({
-                status:true,
-                content:token
+                status: true,
+                content: token
             })
-        }else{
+        } else {
             res.status(401).json({
-                status:false,
-                content:'datos invalidos'
+                status: false,
+                content: 'datos invalidos'
             })
         }
     })

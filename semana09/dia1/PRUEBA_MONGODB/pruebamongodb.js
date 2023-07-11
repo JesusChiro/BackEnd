6c757376fe1c0f64ca245520b09765f764772935
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-07-11 18:31:58 -0500

    dia1 prueba mongodb completado

--- semana09/dia1/PRUEBA_MONGODB/pruebamongodb.js
+++ semana09/dia1/PRUEBA_MONGODB/pruebamongodb.js
@@ -6,6 +6,20 @@ const client = new MongoClient(url)
 async function main() {
     await client.connect()
     console.log("estas conectado a mongodb...")
+
+    const db = client.db('db_codigo_g20')
+    const collection = db.collection('alumnos')
+
+    const insertar = await collection.insertOne(
+        {
+            nombre: "Laura Zuñiga",
+            email: "laura@gmail.com",
+            notas: 20
+        })
+
+    console.log("nueva tarea: ", insertar)
+    const result = await collection.find().toArray()
+    console.log('listado de alumnos ', result)
     return 0
 }
 

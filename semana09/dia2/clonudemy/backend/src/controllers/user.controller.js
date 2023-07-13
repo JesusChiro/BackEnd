08d3f8268caa04159550e8eb4bf56152c2f231bf
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-07-12 22:04:26 -0500

    user auth

--- semana09/dia2/clonudemy/backend/src/controllers/user.controller.js
+++ semana09/dia2/clonudemy/backend/src/controllers/user.controller.js
@@ -3,6 +3,9 @@ const userController = {}
 const bcrypt = require('bcryptjs')
 const userModel = require('../models/user.model')
 
+const { config } = require('../config')
+const jwt = require('jsonwebtoken')
+
 userController.create = async (req, res) => {
     try {
         const hash = await bcrypt.hash(req.body.password, 10)
@@ -19,5 +22,33 @@ userController.create = async (req, res) => {
         })
     }
 }
+
+userController.auth = async (req, res) => {
+    try {
+        dataEmail = req.body.email,
+            dataPassword = req.body.password
+
+        const userAuth = await userModel.findOne({ email: dataEmail })
+        if (await bcrypt.compare(dataPassword, userAuth.password)) {
+            const token = jwt.sign({
+                _id: userAuth._id,
+                email: userAuth.email
+            }, config.jwt_secret,
+                { expiresIn: '1h' })
+            res.status(200).json({
+                'token': token
+            })
+        } else {
+            res.status(404).json({
+                message: 'invalid user or password'
+            })
+        }
+    } catch (err) {
+        res.status(502).json({
+            message: err
+        })
+    }
+}
+
 
 module.exports = userController

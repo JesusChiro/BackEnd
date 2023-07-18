41a9c4b4250281058bdb6c71e7a28b4ce2b3e93f
Jesus Daniel Chiroque Martinez <jesus_chiroque@hotmail.com>
2023-07-17 20:46:26 -0500

    dia5 pm2

---
+++ semana09/dia2/clonudemy/backend/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [{
+    name: 'ms-courses',
+    script: 'src/microservices/ms-courses/index.js'
+  }, {
+    name: 'ms-users',
+    script: 'src/microservices/ms-users/index.js'
+  }
+  ],
+};
